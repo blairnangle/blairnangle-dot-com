9f7b796e6d0c0f69c3ec4c7627504f0875b7d489
Blair Nangle <hi@blairnangle.com>
2022-02-28 17:16:59 +0000

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,5 +77,11 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-124521773-1',
+      },
+    },
   ],
 };
--- src/components/Favicon.jsx
+++ src/components/Favicon.jsx
@@ -8,13 +8,13 @@ function Favicon() {
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="../../static/favicon-32x32.png"
+        href="../../static/favicon.ico"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="../../static/favicon-16x16.png"
+        href="../../static/favicon.ico"
       />
       <meta name="theme-color" content="#ffffff" />
     </Helmet>
