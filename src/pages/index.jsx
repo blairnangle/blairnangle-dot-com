988f88a21d083b961a67f172e6e8be1581207fba
Blair Nangle <hi@blairnangle.com>
2022-03-02 19:01:09 +0000

    Initalise with random photo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,7 +37,7 @@ function Index() {
     return photoCaptionMap[keys[keys.length * Math.random() << 0]];
   };
 
-  const [homePhoto, setHomePhoto] = useState(photoCaptionMap.one);
+  const [homePhoto, setHomePhoto] = useState(randomPhoto());
 
   useEffect(() => {
     setHomePhoto(randomPhoto());
