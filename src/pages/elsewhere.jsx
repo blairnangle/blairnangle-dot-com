c4318443b088da909fbf5b5dfbfffc722a54faf0
Blair Nangle <hi@blairnangle.com>
2022-10-21 20:17:44 +0100

    Add link to Exercism profile; alphabetise /elsewhere links

--- src/pages/elsewhere.jsx
+++ src/pages/elsewhere.jsx
@@ -8,7 +8,17 @@ const elsewhere = () => (
       <p>
         •
         {' '}
+        <XA href="https://exercism.org/profiles/blairnangle">Exercism</XA>
+      </p>
+      <p>
+        •
+        {' '}
         <XA href="https://github.com/blairnangle">GitHub</XA>
+      </p>
+      <p>
+        •
+        {' '}
+        <XA href="https://www.goodreads.com/user/show/74431442-blair-nangle">Goodreads</XA>
       </p>
       <p>
         •
@@ -23,22 +33,17 @@ const elsewhere = () => (
       <p>
         •
         {' '}
+        <XA href="https://linkedin.com/in/blairnangle/">LinkedIn</XA>
+      </p>
+      <p>
+        •
+        {' '}
         <XA href="https://stackoverflow.com/users/4304123/blair-nangle">Stack Overflow</XA>
       </p>
       <p>
         •
         {' '}
         <XA href="https://twitter.com/blairnangle">Twitter</XA>
-      </p>
-      <p>
-        •
-        {' '}
-        <XA href="https://linkedin.com/in/blairnangle/">LinkedIn</XA>
-      </p>
-      <p>
-        •
-        {' '}
-        <XA href="https://www.goodreads.com/user/show/74431442-blair-nangle">Goodreads</XA>
       </p>
     </div>
   </Layout>
