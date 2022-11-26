ba924a5c775a6f5b7b3aaf6f3490b16dbc5fd18a
Blair Nangle <hi@blairnangle.com>
2022-11-26 12:02:13 +0000

    [Infra] Log access; add headers policy

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -35,11 +35,12 @@ resource "aws_cloudfront_distribution" "cdn" {
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 31536000
-    default_ttl            = 31536000
-    max_ttl                = 31536000
-    compress               = true
+    viewer_protocol_policy     = "redirect-to-https"
+    min_ttl                    = 31536000
+    default_ttl                = 31536000
+    max_ttl                    = 31536000
+    compress                   = true
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.headers.id
   }
 
   restrictions {
@@ -52,6 +53,24 @@ resource "aws_cloudfront_distribution" "cdn" {
     acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  depends_on = [
+    aws_cloudfront_origin_access_identity.origin_access_identity,
+    aws_route53_record.validation,
+    aws_s3_bucket.static_hosting
+  ]
+
+  logging_config {
+    bucket          = aws_s3_bucket.static_hosting.bucket_domain_name
+    prefix          = "access-logs/"
+    include_cookies = true
   }
 }
 
