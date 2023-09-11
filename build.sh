ae84666f16fac7e642003a483fbc8d5a4422cb4b
Blair Nangle <hi@blairnangle.com>
2023-09-11 10:43:24 +0100

    [Brain] Add full deployment pipeline for brain.blairnangle.com

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-npm exec gatsby build
+APP="$1"
+
+if [[ "website" == "${APP}" ]]; then
+  npm exec gatsby build
+  elif [[ "brain" == "${APP}" ]]; then
+  npm exec quartz build
+  else
+  echo "Unknown app: ${APP}"
+  exit 1
+fi
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CLOUDFRONT_DISTRIBUTION_ID="$1"
+S3_BUCKET="$1"
+CLOUDFRONT_DISTRIBUTION_ID="$2"
 
-aws s3 sync public/ s3://blairnangle.com
+aws s3 sync public/ "s3://${S3_BUCKET}"
 
 aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --paths /\*
