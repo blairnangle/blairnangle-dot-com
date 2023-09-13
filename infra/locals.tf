c362a0e3ffcab5b275e6cee7a07093b237e2a9fe
Blair Nangle <hi@blairnangle.com>
2023-09-13 10:22:46 +0100

    [Infra] Remove now-redundant resources for `brain.` subdomain
    - Required resources are now defined in https://github.com/blairnangle/brain
    - Or Vercel project settings

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,6 +1,5 @@
 locals {
   cert_subject_alternative_names = [
-    "*.${var.domain}",
-    "*.brain.${var.domain}",
+    "*.${var.domain}"
   ]
 }
--- infra/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "acm_certificate_validation_certificate_arn" {
-  value = aws_acm_certificate_validation.cert_validation.certificate_arn
-}
